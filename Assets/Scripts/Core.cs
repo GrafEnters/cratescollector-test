34ef7b0db392b026284be0d9bcadb5b40eb2a28f
Graf Enters <graf.enters@gmail.com>
2026-02-06 12:40:28 +0400

    removed old scenemanagement code

--- Assets/Scripts/Core.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Core : MonoBehaviour
-{
-    public void Exit() {
-        SceneManager.LoadScene("MenuScene");
-    }
-}
--- Assets/Scripts/Loading.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Loading : MonoBehaviour
-{
-   private void Start() {
-      SceneManager.LoadScene("MenuScene");
-   }
-}
